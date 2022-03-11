1f083c1b7e6f4a2037ca50668373028d430b9666
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2022-03-11 13:22:54 -0500

    config stub

---
+++ _config.js
@@ -0,0 +1,12 @@
+const db = {
+    "host": "host",
+    "user": "user",
+    "password": "pwd",
+    "database": "db",
+    "connectionLimit": 5
+};
+
+const config = {};
+config.db = db;
+
+module.exports = config;

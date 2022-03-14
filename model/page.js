d89709884311e86fcf3465cb2a52c40a5e165cbf
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2022-03-14 10:32:28 -0400

    added page model stub

---
+++ model/page.js
@@ -0,0 +1,25 @@
+const dbConnPool = require('./db');
+
+let Page = {};
+
+Page.getPage = async(key) => {
+
+    let result = {};
+
+    let dbConn = await dbConnPool.getConnection();
+    const rows = await dbConn.query("SELECT pageKey,title,content,dateModified FROM `page` WHERE pageKey = ?", [key]);
+    dbConn.end();
+
+    if (rows.length > 0) {
+        result.status = true;
+        result.data = rows[0];
+    } else {
+        result.status = false;
+    }
+
+    return result;
+};
+
+
+
+module.exports = Page;

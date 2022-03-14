42acd8ab7c3291fae51ddadd4e98164428c64bda
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2022-03-14 11:47:36 -0400

    added edit page template/route (start for page editing)

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 const menuModel = require('../model/menu');
+const pageModel = require('../model/page');
+
+let loadPage = async function(pageKey, req, res, next, edit = false) {
+    if (pageKey != '') {
+        let page = await pageModel.getPage(pageKey);
+        if (page.status) {
+            let menuItems = await menuModel.getItems();
+            menuItems.name = "main";
+            page.edit = edit;
+            res.render('index', { page: page, menu: menuItems });
+            return;
+        }
+    }
+    next();
+}
 
 /* GET home page. */
-router.get('/', async function(req, res) {
-
-    let menuItems = await menuModel.getItems();
-
-
-    menuItems.name = "main";
-
-    //console.log(menuItems);
-    res.render('index', { title: 'Express', menu: menuItems });
+router.get('/', async function(req, res, next) {
+    loadPage('home', req, res, next)
 });
 
+router.get('/page/:pageKey', async(req, res, next) => {
+    let pageKey = req.params.pageKey.trim().toLowerCase();
+    loadPage(pageKey, req, res, next);
+});
+router.get('/page/:pageKey/edit', async(req, res, next) => {
+    let pageKey = req.params.pageKey.trim().toLowerCase();
+    loadPage(pageKey, req, res, next, true);
+});
 module.exports = router;

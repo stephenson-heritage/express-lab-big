e70708917370b49adc9e87c03fd8f8dda181a556
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2022-03-15 08:47:56 -0400

    removed bodyParser

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const path = require('path');
 
 const cookieParser = require('cookie-parser');
@@ -21,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // extended : true
 app.use(express.json());
 
 
